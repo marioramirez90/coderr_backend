631367b3e7ff134858d26d25e3d1c7d3247fb591
marioramirez90 <mario-ramirez@hotmail.de>
2026-09-01 12:58:49 +0200

    Add rest_framework and app configurations to INSTALLED_APPS

--- core/settings.py
+++ core/settings.py
@@ -43,6 +43,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'auth_app',
+    'offers_app',
+    'orders_app',
+    'reviews_app',
+
 ]
 
 MIDDLEWARE = [
