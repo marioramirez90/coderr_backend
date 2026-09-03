4aa71beff744793073286d1979e2cb95c331cd1b
marioramirez90 <mario-ramirez@hotmail.de>
2026-09-03 13:47:52 +0200

    Add custom user admin and user profile admin for enhanced management

--- auth_app/admin.py
+++ auth_app/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import UserProfile
 
-# Register your models here.
+
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = "Profile"
+
+
+try:
+    admin.site.unregister(User)
+except admin.sites.NotRegistered:
+    pass
+
+
+@admin.register(User)
+class CustomUserAdmin(BaseUserAdmin):
+    inlines = [UserProfileInline]
+    list_display = ("id", "username", "email", "first_name", "last_name", "is_staff")
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "type", "location", "tel", "created_at")
+    list_filter = ("type",)
+    search_fields = ("user__username", "user__email", "location")
