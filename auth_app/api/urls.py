e0bcdb68ef57e3f8b9eb83787192a48272f48e51
marioramirez90 <mario-ramirez@hotmail.de>
2026-09-06 22:25:17 +0200

    Add profile detail and list views to API URLs for enhanced user profile management

--- auth_app/api/urls.py
+++ auth_app/api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import RegistrationView, LoginView
+from .views import RegistrationView, LoginView,ProfileDetailView,BusinessProfileListView,CustomerProfileListView
 
 urlpatterns = [
     path("registration/", RegistrationView.as_view(), name="registration"),
     path("login/", LoginView.as_view(), name="login"),
+    path("profile/<int:pk>/", ProfileDetailView.as_view(), name="profile-detail"),
+    path("profiles/business/", BusinessProfileListView.as_view(), name="business-profiles"),
+    path("profiles/customer/", CustomerProfileListView.as_view(), name="customer-profiles"),
 ]
